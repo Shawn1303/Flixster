edaf7a1809841dcfd70d7029c47ab4f9c53aa764
Shawn1303 <65193978+Shawn1303@users.noreply.github.com>
2022-10-02 22:06:32 -0400

    first

---
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -0,0 +1,10 @@
+package com.example.flixster
+
+import com.google.gson.annotations.SerializedName
+
+class Movie {
+    @SerializedName("title")
+    var title: String?= null
+
+    @SerializedName("")
+}
