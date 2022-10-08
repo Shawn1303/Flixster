ec935bc72b4ff39550243b55b09ba587cf5efa67
Shawn1303 <65193978+Shawn1303@users.noreply.github.com>
2022-10-08 05:34:52 -0400

    moviereview

---
+++ app/src/main/java/com/example/flixster/MovieReview.kt
@@ -0,0 +1,11 @@
+package com.example.flixster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieReview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_review)
+    }
+}
